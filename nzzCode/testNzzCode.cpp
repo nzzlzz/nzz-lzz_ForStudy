5585f11b36344fec46fab51975e5c4d6b2d6b20a
nzzlzz <liangjy041030@mail.nwpu.edu.cn>
2025-01-10 19:22:54 +0800

    change

---
+++ nzzCode/testNzzCode.cpp
@@ -0,0 +1,8 @@
+/*************  ✨ Codeium Command ⭐  *************/
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+/******  57ab5cfb-9daf-40bf-ae09-9fcc48cd319b  *******/
