f8d8fd30c79e60ff1151baeef6f8181881a3d4ef
梁景毓 <3445078775@qq.com>
2025-01-13 23:06:32 +0800

    1

--- lzzCode/dgStudy.cpp
+++ lzzCode/dgStudy.cpp
@@ -1,17 +1,29 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int jiecheng(int n){
-    if(n==1){
+int jiecheng(int n)
+{
+    if (n == 0)
+    {
         return 1;
     }
-    n=n*jiecheng(n-1);
+    n = n * jiecheng(n - 1);
     return n;
 }
-int main(){
-    int n,i;
-    cin>>n;
-    i=jiecheng(n);
-    cout<<"last:"<<i<<endl;
+
+int fibonacci(int n){
+    if(n<=1){
+        return 1;
+    }
+    n=fibonacci(n-1)+fibonacci(n-2);
+    return n;
+}
+
+int main()
+{
+    int n, i;
+    cin >> n;
+    i = fibonacci(n);
+    cout << "last:" << i << endl;
     return 0;
 }
